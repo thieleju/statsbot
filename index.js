0c1d5ca5daa413ed72494f8daef9e0ff73323a75
Julian <thiele.juli@gmail.com>
2022-10-01 23:26:57 +0200

    Moved invite link to /invite command

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ const { Client, Collection, GatewayIntentBits } = require("discord.js");
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Read all command files
+// Login to Discord with your client's token
+client.login(process.env.DC_BOT_TOKEN).catch(console.error);
+
+// Read all command files from commands folder (__dirname is the current directory)
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith(".js"));
 
-// Set command files as commands
+// Read command files and add them to the client.commands collection
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
@@ -27,20 +30,25 @@ for (const file of commandFiles) {
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
   console.log("Bot ready and online!");
-  console.log(
-    `Invite link is: https://discord.com/api/oauth2/authorize?client_id=${process.env.DC_BOT_CLIENT_ID}&permissions=${process.env.DC_BOT_PERMISSIONS}&scope=applications.commands%20bot`
-  );
 });
 
+// React to chat interactions
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
 
+  // do nothing if command is not found
   if (!command) return;
 
   try {
+    // Execute the command
     await command.execute(interaction);
+    // Log command usage
+    const timestamp = new Date().toISOString();
+    console.log(
+      `[${timestamp}] Player ${interaction.user.username} used command ${interaction.commandName}`
+    );
   } catch (error) {
     console.error(error);
     await interaction.reply({
@@ -49,6 +57,3 @@ client.on("interactionCreate", async (interaction) => {
     });
   }
 });
-
-// Login to Discord with your client's token
-client.login(process.env.DC_BOT_TOKEN);
