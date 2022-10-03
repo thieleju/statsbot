b61d5bee7119b669b5498222ac2829c293451553
Bikas Kumar <102891869+Bikaskumar572@users.noreply.github.com>
2022-10-03 13:26:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,3 +82,30 @@ client.on("interactionCreate", async (interaction) => {
     });
   }
 });
+
+//for weather
+client.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address.",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
