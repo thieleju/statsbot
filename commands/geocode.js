9ca494895c69f61f2b6afe34296b3e508ec0850c
Bikas Kumar <102891869+Bikaskumar572@users.noreply.github.com>
2022-10-03 13:23:38 +0530

    Create geocode.js

---
+++ commands/geocode.js
@@ -0,0 +1,24 @@
+const request = require("postman-request");
+require("dotenv").config();
+
+const geocode = (address, callback) => {
+  const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=${process.env.accessKey_mapBox}&limit=1`;
+
+  request({ url: geocodeURL, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to location services :/", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find location. Try another search", undefined);
+    } else {
+      callback(undefined, {
+        latitude: response.body.features[0].center[1],
+        longitude: response.body.features[0].center[0],
+        location: response.body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
