165af77d5ae14b49a8d99a039e7a7691d73078f5
Abhishek Patil <abishkpatil@gmail.com>
2022-10-04 01:11:03 +0530

    Added random dog pic command

---
+++ commands/dogpic.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios").default;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("dogpic")
+    .setDescription("Get a random picture of dog."),
+  async execute(interaction) {
+    await axios({
+      method: "get",
+      url: "https://random.dog/woof.json",
+      responseType: "json",
+    })
+      .then((response) => {
+        console.log("Response msg: "+response.data.url)
+        interaction.reply(response.data.url);
+      })
+      .catch((error) => {
+        interaction.reply("Something went wrong!");
+      });
+  },
+};
