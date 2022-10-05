b78915807ee284236f49e3df2013942b2b91004e
Julian <thiele.juli@gmail.com>
2022-10-05 22:20:37 +0200

    Fixed format with prettier

--- commands/series.js
+++ commands/series.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const axios = require("axios").default;
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+const axios = require("axios").default
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -12,53 +12,53 @@ module.exports = {
     )
     .setDescription("Get any tv/web series info"),
   async execute(interaction) {
-    await interaction.deferReply();
-    const name = interaction.options.getString("name");
-    let data;
+    await interaction.deferReply()
+    const name = interaction.options.getString("name")
+    let data
 
     try {
       let res = await axios.get(
         `https://catchtheshow.herokuapp.com/api/search?name=${name}`
-      );
-      data = res.data;
+      )
+      data = res.data
 
       if (!data.length) {
-        await interaction.editReply("No series found!");
-        return;
+        await interaction.editReply("No series found!")
+        return
       }
 
-      let showId = data[0].id;
-      res = await axios.get(`https://catchtheshow.herokuapp.com/api${showId}`);
-      data = res.data;
+      let showId = data[0].id
+      res = await axios.get(`https://catchtheshow.herokuapp.com/api${showId}`)
+      data = res.data
     } catch (error) {
-      await interaction.editReply("Ooops! Something went wrong...");
-      console.log("/series command encountered an error: " + error);
-      return;
+      await interaction.editReply("Ooops! Something went wrong...")
+      console.log("/series command encountered an error: " + error)
+      return
     }
 
-    let s = "";
-    s += data.description + "\n\n";
-    s += `:star: **Rating:** ${data.rating}/5\n`;
-    s += `:mirror_ball: **Status:** ${data.status}\n`;
+    let s = ""
+    s += data.description + "\n\n"
+    s += `:star: **Rating:** ${data.rating}/5\n`
+    s += `:mirror_ball: **Status:** ${data.status}\n`
 
     if (data.previousEpisode) {
-      s += "\n**PREVIOUS EPISODE**\n";
-      s += `:bookmark_tabs: **Name:** ${data.previousEpisode.name}\n`;
-      s += `:calendar: **Date:** ${data.previousEpisode.date.day}-${data.previousEpisode.date.month}-${data.previousEpisode.date.year}\n`;
+      s += "\n**PREVIOUS EPISODE**\n"
+      s += `:bookmark_tabs: **Name:** ${data.previousEpisode.name}\n`
+      s += `:calendar: **Date:** ${data.previousEpisode.date.day}-${data.previousEpisode.date.month}-${data.previousEpisode.date.year}\n`
     }
 
     if (data.nextEpisode) {
-      s += "\n**UPCOMING EPISODE**\n";
-      s += `:bookmark_tabs: **Name:** ${data.nextEpisode.name}\n`;
-      s += `:calendar: **Date:** ${data.nextEpisode.date.day}-${data.nextEpisode.date.month}-${data.nextEpisode.date.year}\n`;
+      s += "\n**UPCOMING EPISODE**\n"
+      s += `:bookmark_tabs: **Name:** ${data.nextEpisode.name}\n`
+      s += `:calendar: **Date:** ${data.nextEpisode.date.day}-${data.nextEpisode.date.month}-${data.nextEpisode.date.year}\n`
     }
 
     const embed = new EmbedBuilder()
       .setColor(0x1089df)
       .setTitle(data.name)
       .setImage("https://" + data.imageUrl.substring(2))
-      .setDescription(s);
+      .setDescription(s)
 
-    await interaction.editReply({ embeds: [embed] });
+    await interaction.editReply({ embeds: [embed] })
   },
-};
+}
