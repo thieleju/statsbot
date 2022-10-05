25b5e08ad6f4102e20ba4ba6f1365b6f60fd10c9
moazfarrukh <maazfarrukh@gmail.com>
2022-10-05 23:54:33 +0500

    add duckpic command

---
+++ commands/duckpic.js
@@ -0,0 +1,20 @@
+const axios = require("axios").default
+module.exports = {
+    data: new SlashCommandBuilder()
+      .setName("duckpic")
+      .setDescription("Get a random picture of a duck."),
+    async execute(interaction) {
+      await axios({
+        method: "get",
+        url: "https://random-d.uk/api/quack",
+        responseType: "json",
+      })
+        .then((response) => {
+          interaction.reply(response.data.url)
+        })
+        .catch((error) => {
+          interaction.reply("problem fetching the image!")
+        })
+    },
+  }
+   //
