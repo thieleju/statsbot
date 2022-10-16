7ac5a8d79125288c51b96bce6bc949d36c85cbf1
Max <Maxi.wolleb@gmail.com>
2022-10-16 21:31:06 +0200

    added whatshouldieat

---
+++ commands/whatshouldieat.js
@@ -0,0 +1,41 @@
+const { default: axios } = require("axios")
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("whatshouldieat")
+    .setDescription("Get a random recipe."),
+  async execute(interaction) {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((req) => {
+        const recipe = req.data.meals[0]
+        var ingredients = ""
+        //since api provides max. 20 ingredients loop trough them
+        for (var i = 1; i < 21; i++) {
+          if (recipe["strMeasure" + i])
+            ingredients +=
+              recipe["strMeasure" + i] +
+              " " +
+              recipe["strIngredient" + i] +
+              "\n"
+        }
+        const message = new EmbedBuilder()
+          .setTitle(recipe?.strMeal)
+          .setThumbnail(recipe?.strMealThumb)
+          .setDescription(recipe?.strSource)
+          .addFields({
+            name: "Ingredients",
+            value: ingredients,
+          })
+          .addFields({
+            name: "Instructions",
+            value: recipe?.strInstructions,
+          })
+        interaction.reply({ embeds: [message] })
+      })
+      .catch((err) => {
+        interaction.reply("The API did not respond!")
+      })
+  },
+}
