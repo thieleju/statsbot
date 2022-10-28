747ae388f4064200cdee184a9a3d48e486217773
Appledora <nazia89@student.sust.edu>
2022-10-29 02:22:07 +0600

    add urban dictionary api

---
+++ commands/urban.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder } = require("discord.js")
+const axios = require("axios").default
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("urban")
+    .setDescription("Get the urban dictionary definition of a give term.")
+    .addStringOption((option) =>
+      option
+        .setName("term")
+        .setDescription('The term to search for. For Example: "dog"')
+        .setRequired(false)
+    ),
+
+  async execute(interaction) {
+    let term = interaction.options.getString("term")
+    await interaction.deferReply({ ephemeral: false })
+    let URL = ""
+    if (term == null) {
+      // Get a random term
+      URL = "https://api.urbandictionary.com/v0/random"
+    } else {
+      const query = new URLSearchParams({ term })
+      URL = `https://api.urbandictionary.com/v0/define?${query}`
+    }
+    console.log("URL: ", URL)
+    await axios({
+      method: "get",
+      url: URL,
+      responseType: "json",
+    })
+      .then(async (res) => {
+        let myres = res.data.list[0]
+        if (myres == null) {
+          await interaction.editReply("No results found!")
+        }
+
+        console.log("res: ", myres.definition)
+
+        const data = `Word: ${myres.word}\nDefinition: ${myres.definition}\nExample: ${myres.example}\nRating: ${myres.thumbs_up} thumbs up. ${myres.thumbs_down} thumbs down.`
+        await interaction.editReply(data)
+      })
+      .catch(async () => {
+        await interaction.editReply(
+          `No results found for **${term}** or API did not respond.`
+        )
+      })
+  },
+}
